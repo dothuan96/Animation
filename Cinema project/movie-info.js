299fcc202c9b70de89f8b9d839ec62a53aac1185
Do Thuan <dov.thuan96@gmail.com>
2017-12-19 00:07:16 +0200

    19/12 - 12:07

--- Cinema project/movie-info.js
+++ Cinema project/movie-info.js
@@ -31,3 +31,60 @@ function panelcur(m) {
   panel_contents[n].classList.add('active');
   panel.style.height = panel_contents[n].offsetHeight + 'px';
 }
+
+/*================MOVIES SHOW ANIMATION============================*/
+/* Check the location of each desired element */
+$('.mov-title').each( function(i){
+    var top_of_object = $(this).offset().top;
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+    /* If the object is completely visible in the window, fade it in */
+    if( bottom_of_window > top_of_object + 100 ){
+      //stage 1
+      $(this).animate({opacity:'0', left:'-700px'}, 1200);
+      $('.table').animate({opacity:'0', top:'-340px'}, 1200);
+      $('.star li.1, .star li.2, .star li.3, .star li.4, .star li.5').animate({opacity:'0'}, 1200);
+      $('.btn.1, .btn.2').animate({opacity:'0'}, 1200);
+      //stage 2
+      $('img').animate({opacity:'1', top:'0'}, 800);
+      $(this).animate({opacity:'1', left:'0'}, 800); console.log('a');
+      $('.table').animate({opacity:'0', top:'-340px'}, 800);
+      $('.star li.1, .star li.2, .star li.3, .star li.4, .star li.5').animate({opacity:'0'}, 800);
+      $('.btn.1, .btn.2').animate({opacity:'0'}, 800);
+      //stage 3
+      $('.table').animate({opacity:'1', top:'0'}, 800);
+      $('.star li.1, .star li.2, .star li.3, .star li.4, .star li.5').animate({opacity:'0'}, 800);
+      $('.btn.1, .btn.2').animate({opacity:'0'}, 800);
+      //stage 4
+      $('.star li.1').animate({opacity:'1'}, 600);
+      $('.star li.2').animate({opacity:'1'}, 600*2);
+      $('.star li.3').animate({opacity:'1'}, 600*3);
+      $('.star li.4').animate({opacity:'1'}, 600*4);
+      $('.star li.5').animate({opacity:'1'}, 600*5);
+
+      $('.star li.1').animate({opacity:'1'}, 400);
+      $('.star li.2').animate({opacity:'1'}, 400);
+      $('.star li.3').animate({opacity:'1'}, 400);
+      $('.star li.4').animate({opacity:'1'}, 400);
+      $('.star li.5').animate({opacity:'1'}, 400);
+      //stage 5
+      $('.btn.1').animate({opacity:'0'}, 600);
+      $('.btn.2').animate({opacity:'0'}, 600*2);
+
+      $('.btn.1').animate({opacity:'1'}, 600);
+      $('.btn.2').animate({opacity:'1'}, 600);
+    }
+});
+
+/*=================TAB PANEL ANIMATION WHEN SCROLL DOWN===========================*/
+/* Check the location of each desired element */
+$(window).scroll( function(){
+  $('.tabs').each( function(i){
+      var top_of_object = $(this).offset().top;
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      /* If the object is completely visible in the window, fade it in */
+      if( bottom_of_window > top_of_object){
+        $(this).animate({opacity:'1'}, 800);
+        $('.panels').animate({opacity:'1', top:'0'}, 800);
+      }
+  });
+});
